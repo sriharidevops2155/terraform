db2c942dee088013237b2f69e6994c24dd03c67a
sriharibandi <shreharii@gmail.com>
2025-07-13 08:52:38 +0530

    terraform

---
+++ multi-account/sg.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all_dev"
+  description = "Allow all traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+  egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+  tags = {
+    Name = "allow-all-dev"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all_prod"
+  description = "Allow all traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+  egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+  tags = {
+    Name = "allow_all-prod"
+  }
+}
